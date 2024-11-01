5dd3e526184bf458267a93734240ae6a8a332f06
NagaprasadVr <nagaprasadvr246@gmail.com>
2024-11-01 10:02:06 +0530

    modify config for vercel new

--- dist/entrypoint.js
+++ dist/entrypoint.js
@@ -1,12 +1,5 @@
 import { Hono } from "hono";
-import {
-  auth,
-  merkleTree,
-  mint,
-  peerReview,
-  research,
-  researcherProfile,
-} from "./server/routes/index.js";
+import { auth, merkleTree, mint, peerReview, research, researcherProfile, } from "./server/routes/index.js";
 import { prettyJSON } from "hono/pretty-json";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
@@ -23,7 +16,7 @@ api.use("/", cors());
 api.use(logger());
 api.use(prettyJSON());
 api.get("/", (c) => {
-  return c.text("Hello from deResearcher API, please use the /api endpoint");
+    return c.text("Hello from deResearcher API, please use the /api endpoint");
 });
 api.route("/research", research.route);
 api.route("/researcher-profile", researcherProfile.route);
@@ -33,6 +26,6 @@ api.route("/merkle-tree", merkleTree.route);
 api.route("/auth", auth.route);
 console.log(`Server is running on port http://localhost:${5000}`);
 serve({
-  fetch: api.fetch,
-  port: 5000,
+    fetch: api.fetch,
+    port: 5000,
 });
